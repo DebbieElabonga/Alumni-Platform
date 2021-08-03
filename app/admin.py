80cf381209ffeae19be3f5111a5d10af2f8e945a
Benson <bensongathu23@gmail.com>
2021-08-03 16:14:04 +0300

    user can share stories

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Stories,UserProfile,TechNews
 # Register your models here.
+
+admin.site.register(Stories)
+admin.site.register(UserProfile)
+admin.site.register(TechNews)
