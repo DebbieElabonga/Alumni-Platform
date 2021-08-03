2ca7686f62b5c2aad9d5070268891563ed453cc5
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-08-03 17:19:13 +0300

    add url path for the profile function

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     url('register/',views.signup, name='registration'),
     url('login/', auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    url('profile/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
   urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
