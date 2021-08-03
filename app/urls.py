c503dd48e585b4662f937456f76b5aadae064cce
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-08-03 20:46:22 +0300

    add base.html, cohort.html

--- app/urls.py
+++ app/urls.py
@@ -11,6 +11,7 @@ urlpatterns=[
     url('login/', auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
     url('profile/', views.profile, name='profile'),
+    url('new-cohort/', views.cohort, name='new-cohort'),
 ]
 if settings.DEBUG:
   urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
