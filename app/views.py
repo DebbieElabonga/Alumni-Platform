5d5d4ddfecf61f8fd24744e82b3b5884fe662152
Caleb Odinga <calemasanga@gmail.com>
2021-08-05 14:24:04 +0300

    changes redirect on login to home

--- app/views.py
+++ app/views.py
@@ -25,12 +25,12 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('login')
+            return redirect('index')
     else:
         form = SignupForm()
     return render(request, 'registration/registration_form.html', {'form': form})
 
-@login_required(login_url='/accounts/login/')    
+   
 def profile(request):
     if request.method == 'POST':
         profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user)
@@ -39,7 +39,10 @@ def profile(request):
             return redirect('index')
     else:
         profile_form = UserProfileForm(instance=request.user)
-    return render(request, 'user_profile.html',{ "profile_form": profile_form})
+        context = { 
+            "profile_form": profile_form
+            }
+    return render(request, 'user_profile.html',context)
 
 def cohort(request):
     if request.method == 'POST':
