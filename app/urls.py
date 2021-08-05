3278fb9367d5574fd6f08e666968c102a73c0e59
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-08-05 14:13:36 +0300

    pycache

--- app/urls.py
+++ app/urls.py
@@ -19,9 +19,8 @@ urlpatterns=[
   path("story/",views.create_story,name="story"),
   url(r'discussion', views.Discussion, name='discussion'),
   url(r'fundraiser', views.Fundraiser, name='fundraiser'),   
-  url(r'^meet_collegues/$', views.meet_collegues, name = 'meet_collegues'),
   path("news/",views.TechNews,name="technews"),
-  url(r'^admin_dashboard/$', views.summary, name = 'admin_dashboard'),
+  
 ]
 if settings.DEBUG:
   urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
