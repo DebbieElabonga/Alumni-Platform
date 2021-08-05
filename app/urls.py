0169fa43aa00c2c1454b118371893fc7480077be
Caleb Odinga <calemasanga@gmail.com>
2021-08-05 13:18:19 +0300

    changes path to url in app.urls.py, removes admin_dashboard duplicat url

--- app/urls.py
+++ app/urls.py
@@ -7,20 +7,19 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path("",views.index,name="index"),
-  url('register/',views.signup, name='registration'),
-  url('login/', auth_views.LoginView.as_view(), name='login'),
-  url('logout/',auth_views.LogoutView.as_view(), name='logout'),
-  url('profile/', views.profile, name='profile'),
-  url('new-cohort/', views.cohort, name='new-cohort'),
+  url(r"",views.index,name="index"),
+  url(r'register/',views.signup, name='registration'),
+  url(r'login/', auth_views.LoginView.as_view(), name='login'),
+  url(r'logout/',auth_views.LogoutView.as_view(), name='logout'),
+  url(r'profile/', views.profile, name='profile'),
+  url(r'new-cohort/', views.cohort, name='new-cohort'),
   url(r'^meet_collegues/$', views.meet_collegues, name = 'meet_collegues'),
   url(r'^single_idea/(\d+)/$', views.single_idea, name = 'single_idea'),
-  url(r'^admin_dashboard/$', views.admin_dashboard, name = 'admin_dashboard'),
-  path("story/",views.create_story,name="story"),
+  url(r"story/",views.create_story,name="story"),
   url(r'discussion', views.Discussion, name='discussion'),
   url(r'fundraiser', views.Fundraiser, name='fundraiser'),   
   url(r'^meet_collegues/$', views.meet_collegues, name = 'meet_collegues'),
-  path("news/",views.TechNews,name="technews"),
+  url(r"news/",views.TechNews,name="technews"),
   url(r'^admin_dashboard/$', views.summary, name = 'admin_dashboard'),
 ]
 if settings.DEBUG:
