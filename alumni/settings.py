68c104ab184e6073c1a221555fd45cb507f363c4
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-08-12 10:08:22 +0300

    add database settings

--- alumni/settings.py
+++ alumni/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'alumni.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': 'alumni',
+        'USER': 'postgres',
+        'PASSWORD': 'chebichii1',
+        'HOST': 'localhost',
     }
 }
 
